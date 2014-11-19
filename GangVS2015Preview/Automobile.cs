dd4122cb7638f85c5a721b0c47ed8cf10cf049ca
PauloPolaco <PBagadzinski@appgraft.com>
2014-11-19 14:54:37 -0500

    Initial commit.

---
+++ GangVS2015Preview/Automobile.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GangVS2015Preview
+{
+    internal sealed class Automobile
+    {
+        public Guid Id { get; } = Guid.NewGuid();
+        public string Make { get; } = "Toyota";
+        public string Model { get; } = "Camry";
+        public DateTime ManufactureDate { get; } = DateTime.UtcNow;
+
+        public Automobile()
+        {
+            // no code
+        }
+
+        public Automobile(string make, string model, DateTime manufactureDate)
+        {
+            this.Make = make;
+            this.Model = model;
+            this.ManufactureDate = manufactureDate;
+        }
+    }
+}
---
+++ GangVS2015Preview/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GangVS2015Preview
+{
+    public sealed class Program
+    {
+        /// <summary>
+        /// System.Math now brings in static
+        /// classes with static methods.
+        /// </summary>
+        private double m_distance => Math.Sqrt(64);
+        
+        /// <summary>
+        /// You no longer need to do use curly 
+        /// braces when initializing dictionaries.
+        /// { 200, "Okay" }
+        /// { 403, "Forbidden" }
+        /// </summary>
+        private IDictionary m_standardResponseCodes = new Dictionary<int, string>()
+        {
+            [200] = "Okay",
+            [403] = "Forbidden",
+            [404] = "Page not found",
+            [500] = "Internal server error"
+        };
+
+        static void Main(string[] args)
+        {
+            FormattedStringEscapeBreakpoint();
+            IEnumerable<Automobile> cars = AutoPropInitializers();
+            ExceptionFilters(cars);
+            NullChecks(cars);
+
+            Console.WriteLine("Press the ANY key to continue ...");
+            Console.ReadKey();
+        }
+
+        private static int FormattedStringEscapeBreakpoint()
+        {
+            int sum = 0;
+            
+            for (int iterator = 0; iterator < 10; iterator++)
+            {
+                Console.WriteLine("iter = {0} | sum = {1}", iterator, sum);
+                Console.WriteLine("iter = \{iterator} | sum = \{sum}");
+
+                sum++;
+            }
+
+            return sum;
+        }
+
+        private static IEnumerable<Automobile> AutoPropInitializers()
+        {
+            IList<Automobile> cars = new List<Automobile>()
+            {
+                new Automobile("Ford", "Focus", new DateTime(2014, 8, 15)),
+                new Automobile("CompanyName", null, DateTime.UtcNow),
+                new Automobile(),
+                null
+            };
+
+            return cars;
+        }
+        
+        private static void ExceptionFilters(IEnumerable<Automobile> cars)
+        {
+            foreach (Automobile car in cars)
+            {
+                try
+                {
+                    Console.WriteLine("Model = {0}", car.Model);
+                }
+                catch (Exception ex) if (car == null)
+                {
+                    Console.WriteLine("Car is null!");
+                }
+            }
+        }
+
+        private static void NullChecks(IEnumerable<Automobile> cars)
+        {
+            foreach (Automobile car in cars)
+            {
+                // old way
+                if(car != null && car.Model != null)
+                {
+                    Console.WriteLine("Model = " + car.Model.ToString());
+                }
+
+                // new way
+                Console.WriteLine("Model = " + car?.Model?.ToString());
+            }
+        }
+    }
+}
